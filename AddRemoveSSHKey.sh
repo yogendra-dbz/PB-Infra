b37b6dceae056c6bb66505bc490a6d7222ac95d3
yogendra8singh <yogendra8singh@gmail.com>
2018-03-30 15:32:31 +0530

    Update AddRemoveSSHKey.sh

--- AddRemoveSSHKey.sh
+++ AddRemoveSSHKey.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-ssh-keygen -f ~/.ssh/known_hosts -R 35.168.203.14
-ssh-keygen -f ~/.ssh/known_hosts -R 35.172.249.241
-ssh-keygen -f ~/.ssh/known_hosts -R 18.233.41.155
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.168.203.14
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.172.249.241
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 18.233.41.155
 
-ssh-keyscan -H  35.168.203.14 >>  ~/.ssh/known_hosts
-ssh-keyscan -H 35.172.249.241 >>  ~/.ssh/known_hosts
-ssh-keyscan -H 18.233.41.155 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H  35.168.203.14 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 35.172.249.241 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 18.233.41.155 >>  ~/.ssh/known_hosts
 
-echo "$PBPASS" > `pwd`/my_password.txt
+sudo echo "$PBPASS" > `pwd`/my_password.txt
