9dda5c228c54989e5fa8939f3e8507ac8e19d5f9
yogendra8singh <yogendra8singh@gmail.com>
2018-05-01 14:32:11 +0530

    Update the EIPs

--- AddRemoveSSHKey.sh
+++ AddRemoveSSHKey.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.168.203.14
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.172.249.241
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 18.233.41.155
-sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.172.96.54
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 18.208.27.151
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 18.234.17.242
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.174.145.65
+sudo ssh-keygen -f ~/.ssh/known_hosts -R 35.174.79.128
 
-sudo ssh-keyscan -H  35.168.203.14 >>  ~/.ssh/known_hosts
-sudo ssh-keyscan -H 35.172.249.241 >>  ~/.ssh/known_hosts
-sudo ssh-keyscan -H 18.233.41.155 >>  ~/.ssh/known_hosts
-sudo ssh-keyscan -H 35.172.96.54 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H  18.208.27.151 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 18.234.17.242 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 35.174.145.65 >>  ~/.ssh/known_hosts
+sudo ssh-keyscan -H 35.174.79.128 >>  ~/.ssh/known_hosts
 
 sudo echo "$PBPASS" > `pwd`/my_password.txt
